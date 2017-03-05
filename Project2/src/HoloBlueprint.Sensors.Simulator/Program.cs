ae2036b4a7dc5e766a46fca20226a1e39bd51ae5
manishrke <sharma_manish@msn.com>
2017-03-05 09:47:03 +0530

    Restructured the solution, along with Readme file updates

--- Project2/src/HoloBlueprint.Sensors.Simulator/Program.cs
+++ Project2/src/HoloBlueprint.Sensors.Simulator/Program.cs
@@ -23,9 +23,6 @@
 
         private static async Task SendMessagesToEventHub(int numMessagesToSend)
         {
-            // Creates an EventHubsConnectionStringBuilder object from a the connection string, and sets the EntityPath.
-            // Typically the connection string should have the Entity Path in it, but for the sake of this simple scenario
-            // we are using the connection string from the namespace.
             var connectionStringBuilder = new EventHubsConnectionStringBuilder(EhConnectionString)
             {
                 EntityPath = EhEntityPath
